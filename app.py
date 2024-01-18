9d912fdf96e46dff55edcd61bead8cb01dcce37a
r-leyshon <richard.leyshon@ons.gov.uk>
2024-01-18 17:32:36 +0000

    feat: Try except connection error & log error

--- app.py
+++ app.py
@@ -45,12 +45,17 @@ def server(input, output, session):
     @reactive.event(input.predict)
     def pred():
         logging.info("Request Made")
-        r = requests.post(api_url, json = vals())
-        logging.info("Request Returned")
-        if r.status_code != 200:
-            logging.error(f"HTTP error {r.status_code} returned: {r.reason}")
-        else:
-            return r.json().get('predict')[0]
+        try:
+            r = requests.post(api_url, json = vals())
+            logging.info("Request Returned")
+            if r.status_code != 200:
+                logging.error(f"HTTP error {r.status_code} returned: {r.reason}")
+            else:
+                return r.json().get('predict')[0]
+
+        except requests.ConnectionError:
+            logging.error("Failed to connect to VetiverAPI. Have you run launch-api.py?")
+            return 0
 
     @output
     @render.text
